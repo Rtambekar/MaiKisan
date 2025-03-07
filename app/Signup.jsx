240bc277f4abf74251c7d27753fe525d94608730
Rugwed tambekar <rugwedrjt21@gmail.com>
2025-03-07 18:17:30 +0530

    sucessfully naviagtion and verify token 01

---
+++ app/Signup.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <View style={{backgroundColor: "#FFFFFF", flex:1, justifyContent: "center", alignItems: "center"}}>
+      <Text style= {{fontSize: 50}}>Signup</Text>
+    </View>
+  )
+}
+
+export default Signup
---
+++ app/explore.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const explore = () => {
+  return (
+    <View style={{backgroundColor: "#FFFFFF"}}>
+      <Text>explore</Text>
+    </View>
+  )
+}
+
+export default explore
